454c05a8fdbf8e67ff1bad8215d77cd2cb6f187a
José María Coria <ExB8Ch@gmail.com>
2022-10-07 15:11:33 -0300

    character limit counter

--- src/components/TextArea.jsx
+++ src/components/TextArea.jsx
@@ -1,8 +1,9 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
 const Container = styled.div`
+  position: relative;
   width: 100%;
   height: 100%;
 `
@@ -15,8 +16,18 @@ const Text = styled.textarea`
   text-align: center;
   font-size: 19px;
 `
+const Counter = styled.div`
+  width: 7ch;
+  position: absolute;
+  top: 51px;
+  left: 1102px;
+  font-size: 15px;
+  color: var(${props => props.counter === 200 ? '--warning' : '--limit'});
+  text-align: end;
+`
 
 export default function TextArea ({ addTask }) {
+  const [limitCounter, setLimitCounter] = useState(0)
   const textAreaRef = useRef()
 
   const keydown = e => {
@@ -29,7 +40,8 @@ export default function TextArea ({ addTask }) {
 
   return (
     <Container>
-      <Text ref={textAreaRef} maxLength={200} onKeyDown={keydown} placeholder='Enter task content here.&#10;Press enter to add.' />
+      <Counter counter={limitCounter}>{limitCounter}/200</Counter>
+      <Text ref={textAreaRef} maxLength={200} onKeyDown={keydown} placeholder='Enter task content here.&#10;Press enter to add.' onChange={() => setLimitCounter(textAreaRef.current.value.length)} />
     </Container>
   )
 }
